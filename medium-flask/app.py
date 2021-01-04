86707a12969334db6aae16830cc7bc4a6390342f
Gui-go <guilhermeviegas1993@gmail.com>
2021-01-04 02:29:19 -0300

    update

--- medium-flask/app.py
+++ medium-flask/app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hey, we have Flask in a GKE!'
 
+@app.route('/<name>')
+def hello_name(name):
+    return 'Hello, ' + name + '!'
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
